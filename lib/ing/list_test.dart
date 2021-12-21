8d986be30cb4d7b60b870ea76d08654ce1370ac8
eejoy1212 <79134254+eejoy1212@users.noreply.github.com>
2021-12-22 02:50:42 +0900

    [WGS_VIEWER][이원희][21.12.22]-리스트에 체크박스 ui추가

--- lib/ing/list_test.dart
+++ lib/ing/list_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:wgs_viewer/controller/check_box_ctrl.dart';
 import 'package:wgs_viewer/controller/file_ctrl.dart';
+import 'package:wgs_viewer/model/checkbox_model.dart';
 
 class ListCtrl extends GetxController {
   static ListCtrl get to => Get.find();
@@ -19,7 +21,7 @@ class ListCtrl extends GetxController {
 
 class ListTest extends GetView<ListCtrl> {
   final ScrollController scrollCtrl = ScrollController();
-  ListTest({Key? key}) : super(key: key);
+  final allChecked = CheckBoxModel(title: 'All Select : ');
 //로그뷰 참고
   @override
   Widget build(BuildContext context) {
@@ -28,31 +30,78 @@ class ListTest extends GetView<ListCtrl> {
       return SafeArea(
         child: Scrollbar(
           child: SingleChildScrollView(
-              child: ListView.builder(
-            reverse: true,
-            shrinkWrap: true,
-            controller: scrollCtrl,
-            itemCount: FilePickerCtrl.to.selectedFileName.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ListTile(
-                title: Column(
+              child: Column(
+            children: [
+              ListTile(
+                horizontalTitleGap: 200,
+                onTap: () => onAllClicked(allChecked),
+                title: Row(
                   children: [
                     Text(
-                      '${FilePickerCtrl.to.selectedFileName[index]}',
+                      allChecked.title,
                       style: TextStyle(
-                        color: index ==
-                                FilePickerCtrl.to.selectedFileName.length - 1
-                            ? Colors.red
-                            : Colors.black,
+                        fontWeight: FontWeight.bold,
                       ),
+                    ),
+                    Checkbox(
+                      checkColor: Colors.black,
+                      value: allChecked.value,
+                      onChanged: (value) => onAllClicked(allChecked),
                     ),
                   ],
                 ),
-              );
-            },
+                leading: Text(
+                  'File name',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Divider(),
+              ListView.builder(
+                reverse: true,
+                shrinkWrap: true,
+                controller: scrollCtrl,
+                itemCount: FilePickerCtrl.to.selectedFileName.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                      onTap: () {
+                        CheckBoxCtrl.to.isChecked.value =
+                            !CheckBoxCtrl.to.isChecked.value;
+                      },
+                      title: Checkbox(
+                        checkColor: Colors.black,
+                        value: CheckBoxCtrl.to.isChecked.value,
+                        onChanged: (value) {
+                          CheckBoxCtrl.to.isChecked.value =
+                              !CheckBoxCtrl.to.isChecked.value;
+                        },
+                      ),
+                      leading: Text(
+                        '${FilePickerCtrl.to.selectedFileName[index]}',
+                        style: TextStyle(
+                          color: index ==
+                                  FilePickerCtrl.to.selectedFileName.length - 1
+                              ? Colors.red
+                              : Colors.black,
+                        ),
+                      ));
+                },
+              ),
+            ],
           )),
         ),
       );
     });
+  }
+
+  onAllClicked(CheckBoxModel ckbItem) {
+    final newValue = !ckbItem.value;
+    // setState(() {
+    //   ckbItem.value = newValue;
+    //   checkboxList.forEach((element) {
+    //     element.value = newValue;
+    //   });
+    // });
   }
 }
