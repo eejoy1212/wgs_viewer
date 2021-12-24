c9137fc397e7209bc04574dab3f1021f30043017
eejoy1212 <eejoy1212@gmail.com>
2021-12-24 18:57:54 +0900

    [WGS_VIEWER][이원희][21.12.24]-flutter_translator뭔지 테스트

---
+++ lib/ing/translator_test.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_translator/flutter_translator.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final TranslatorGenerator _translator = TranslatorGenerator.instance;
+  final Map<String, dynamic> ko = {'title': 'Korean Title From Map'};
+  final Map<String, dynamic> en = {'title': 'English Title From Map'};
+  // final Map<String, dynamic> km = {'title': 'Khmer Title From Map'};
+  final Map<String, dynamic> ja = {'title': 'Japanese Title From Map'};
+
+  @override
+  void initState() {
+    // _translator.init(
+    //   supportedLanguageCodes: ['en', 'km', 'ja'],
+    //   initLanguageCode: 'km',
+    // );
+    _translator.initWithMap(
+      mapLocales: [
+        MapLocale('ko', ko),
+        MapLocale('en', en),
+        // MapLocale('km', km),
+        MapLocale('ja', ja),
+      ],
+      initLanguageCode: 'ko',
+    );
+    _translator.onTranslatedLanguage = _onTranslatedLanguage;
+    super.initState();
+  }
+
+  void _onTranslatedLanguage(Locale? locale) {
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      supportedLocales: _translator.supportedLocales,
+      localizationsDelegates: _translator.localizationsDelegates,
+      home: SettingsScreen(),
+    );
+  }
+}
+
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  final TranslatorGenerator _translator = TranslatorGenerator.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(_translator.getString(context, 'title'))),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Current language is: ${_translator.getLanguageName()}'),
+            const SizedBox(height: 64.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    child: const Text('English'),
+                    onPressed: () {
+                      _translator.translate('en');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    child: const Text('Korean'),
+                    onPressed: () {
+                      _translator.translate('ko');
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: ElevatedButton(
+                    child: const Text('Japanese'),
+                    onPressed: () {
+                      _translator.translate('ja', save: false);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
